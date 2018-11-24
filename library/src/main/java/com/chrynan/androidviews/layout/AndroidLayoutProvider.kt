50412d1e4b26816387dd61f6461783e3446c1770
Chris <ckeenan@starry.com>
2018-11-24 15:56:46 -0500

    Removed test code

--- library/src/main/java/com/chrynan/androidviews/layout/AndroidLayoutProvider.kt
+++ library/src/main/java/com/chrynan/androidviews/layout/AndroidLayoutProvider.kt
@@ -2,5 +2,17 @@
 
 package com.chrynan.androidviews.layout
 
+import android.app.Activity
+import android.view.ViewGroup
+
 object AndroidLayoutProvider {
+
+    fun <V : ViewGroup, P : ViewGroup.LayoutParams> layout(activity: Activity, layout: AndroidLayout) {
+        activity.setContentView(layout.onCreateLayout<V, P>(activity).viewGroup)
+    }
+
+    fun <V : ViewGroup, P : ViewGroup.LayoutParams> layout(parentView: ViewGroup, layout: AndroidLayout) {
+        parentView.removeAllViews()
+        parentView.addView(layout.onCreateLayout<V, P>(parentView.context).viewGroup)
+    }
 }
