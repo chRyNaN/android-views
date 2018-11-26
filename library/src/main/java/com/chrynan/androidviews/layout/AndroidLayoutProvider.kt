3e0f2c3f87c4dd33ac36dc2c2ee35f9374c92259
Chris <ckeenan@starry.com>
2018-11-26 12:01:53 -0500

    Removed more unused generic type parameters in AndroidLayoutProvider

--- library/src/main/java/com/chrynan/androidviews/layout/AndroidLayoutProvider.kt
+++ library/src/main/java/com/chrynan/androidviews/layout/AndroidLayoutProvider.kt
@@ -7,20 +7,20 @@ import android.view.ViewGroup
 
 object AndroidLayoutProvider {
 
-    fun <V : ViewGroup, P : ViewGroup.LayoutParams> layout(activity: Activity, layout: AndroidLayout) {
+    fun layout(activity: Activity, layout: AndroidLayout) {
         activity.setContentView(layout.onCreateLayout(activity).viewGroup)
     }
 
-    fun <V : ViewGroup, P : ViewGroup.LayoutParams> layout(parentView: ViewGroup, layout: AndroidLayout) {
+    fun layout(parentView: ViewGroup, layout: AndroidLayout) {
         parentView.removeAllViews()
         parentView.addView(layout.onCreateLayout(parentView.context).viewGroup)
     }
 
-    fun <T, V : ViewGroup, P : ViewGroup.LayoutParams> render(activity: Activity, layout: AndroidRenderLayout<T>, item: T) {
+    fun <T> render(activity: Activity, layout: AndroidRenderLayout<T>, item: T) {
         activity.setContentView(layout.onRenderLayout(activity, item).viewGroup)
     }
 
-    fun <T, V : ViewGroup, P : ViewGroup.LayoutParams> render(parentView: ViewGroup, layout: AndroidRenderLayout<T>, item: T) {
+    fun <T> render(parentView: ViewGroup, layout: AndroidRenderLayout<T>, item: T) {
         parentView.removeAllViews()
         parentView.addView(layout.onRenderLayout(parentView.context, item).viewGroup)
     }
