805711c7d73a180f5879048140fa8de2d404b34b
Chris <ckeenan@starry.com>
2018-12-07 13:33:29 -0500

    Updated AndroidLayoutProvider functions to return ViewGroup

--- library/src/main/java/com/chrynan/androidviews/layout/AndroidLayoutProvider.kt
+++ library/src/main/java/com/chrynan/androidviews/layout/AndroidLayoutProvider.kt
@@ -7,33 +7,41 @@ import android.view.ViewGroup
 
 object AndroidLayoutProvider {
 
-    fun layout(activity: Activity, layout: AndroidLayout) {
+    fun layout(activity: Activity, layout: AndroidLayout): ViewGroup {
         layout.onBeforeCreateLayout()
-        activity.setContentView(layout.onCreateLayout(activity).viewGroup)
+        val viewGroup = layout.onCreateLayout(activity).viewGroup
+        activity.setContentView(viewGroup)
         layout.onLayoutCreated()
+        return viewGroup
     }
 
-    fun layout(parentView: ViewGroup, layout: AndroidLayout) {
+    fun layout(parentView: ViewGroup, layout: AndroidLayout): ViewGroup {
         parentView.removeAllViews()
         layout.onBeforeCreateLayout()
-        parentView.addView(layout.onCreateLayout(parentView.context).viewGroup)
+        val viewGroup = layout.onCreateLayout(parentView.context).viewGroup
+        parentView.addView(viewGroup)
         layout.onLayoutCreated()
+        return viewGroup
     }
 
-    fun <T> render(activity: Activity, layout: AndroidRenderLayout<T>, item: T) {
+    fun <T> render(activity: Activity, layout: AndroidRenderLayout<T>, item: T): ViewGroup {
         layout.onBeforeCreateLayout()
         layout.onBeforeRender()
-        activity.setContentView(layout.onRenderLayout(activity, item).viewGroup)
+        val viewGroup = layout.onRenderLayout(activity, item).viewGroup
+        activity.setContentView(viewGroup)
         layout.onLayoutCreated()
         layout.onLayoutRendered()
+        return viewGroup
     }
 
-    fun <T> render(parentView: ViewGroup, layout: AndroidRenderLayout<T>, item: T) {
+    fun <T> render(parentView: ViewGroup, layout: AndroidRenderLayout<T>, item: T): ViewGroup {
         parentView.removeAllViews()
         layout.onBeforeCreateLayout()
         layout.onBeforeRender()
-        parentView.addView(layout.onRenderLayout(parentView.context, item).viewGroup)
+        val viewGroup = layout.onRenderLayout(parentView.context, item).viewGroup
+        parentView.addView(viewGroup)
         layout.onLayoutCreated()
         layout.onLayoutRendered()
+        return viewGroup
     }
 }
